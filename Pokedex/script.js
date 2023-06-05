2b6c07645e6cd46ce297627f42a2b106decfa1c9
tiagolandeiroto <tiagolandeiroto@gmail.com>
2023-06-06 00:53:13 +0100

    api implementation

--- Pokedex/script.js
+++ Pokedex/script.js
@@ -1,1 +1,15 @@
+const pokemon_name_call = "https://pokeapi.co/api/v2/pokemon/";
 
+function getName() {
+  let pokemon_name = document.getElementById("pokemon-name").value;
+  document.getElementById("image").innerHTML = pokemon_name;
+
+  let requestOptions = {
+    method: "GET",
+    redirect: "follow",
+  };
+
+  fetch(pokemon_name_call + pokemon_name).then((response) =>
+    response.text().then((text) => console.log(text))
+  );
+}
