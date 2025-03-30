09716377bc045510a81c81fe8fb5dabbce34dfaa
tiagolandeiroto <tiagolandeiroto@gmail.com>
2025-03-30 11:08:00 +0100

    Added tooltip for the type images

--- Pokedex/script.js
+++ Pokedex/script.js
@@ -115,10 +115,14 @@ function updatePokemon(pokemon) {
   // Update type images
   if (pokemon.types.length === 1) {
     document.getElementById("type_image").setAttribute("src", getLogo(pokemon.types[0].type.name));
+    document.getElementById("type_image").title = pokemon.types[0].type.name.charAt(0).toUpperCase() + pokemon.types[0].type.name.slice(1);
     document.getElementById("type_image2").style.display = "none";
+
   } else if (pokemon.types.length === 2) {
     document.getElementById("type_image").setAttribute("src", getLogo(pokemon.types[0].type.name));
+    document.getElementById("type_image").title = pokemon.types[0].type.name.charAt(0).toUpperCase() + pokemon.types[0].type.name.slice(1);
     document.getElementById("type_image2").setAttribute("src", getLogo(pokemon.types[1].type.name));
+    document.getElementById("type_image2").title = pokemon.types[1].type.name.charAt(0).toUpperCase() + pokemon.types[1].type.name.slice(1);
     document.getElementById("type_image2").style.display = "inline";
   }
 
